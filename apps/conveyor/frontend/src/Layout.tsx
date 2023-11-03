e9e337514963e1b52da65de470aaa2b65792f1cd
changwonyoon <cwyoon@semi-ts.com>
2023-11-03 15:51:31 +0900

    feat: layout 뼈대

--- apps/conveyor/frontend/src/Layout.tsx
+++ apps/conveyor/frontend/src/Layout.tsx
@@ -10,13 +10,15 @@ const Layout = () => {
   /* ======   function    ====== */
   /* ======   useEffect   ====== */
   return (
-    <div className="flex min-h-screen">
-      <nav className="bg-red-50">네비</nav>
-      <div>
-        <header className="bg-red-400">헤더</header>
-        <main>
+    <div className="flex min-h-screen overflow-x-hidden">
+      <nav className="bg-red-50 basis-28">네비</nav>
+      <div className="flex-auto flex flex-col">
+        <header className="bg-red-400 sticky top-0 z-50">헤더</header>
+        <main className="flex-auto">
           <Outlet />
+          {/* <div className="h-[9999px]"></div> */}
         </main>
+        <footer className="bg-slate-600">© 2023 semi-ts, Inc. all rights reserved.</footer>
       </div>
     </div>
   );
