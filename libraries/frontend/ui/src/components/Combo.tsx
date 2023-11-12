4507b409af7cb6d83fe3a99ad92ce919fbd65ab6
changwonyoon <cwyoon@semi-ts.com>
2023-11-13 08:42:47 +0900

    feat: update

--- libraries/frontend/ui/src/components/Combo.tsx
+++ libraries/frontend/ui/src/components/Combo.tsx
@@ -17,12 +17,14 @@ export interface ComboProps extends SelectHTMLAttributes<HTMLSelectElement> {
 }
 /* ======    global     ====== */
 const logger = createLogger('components/Combo');
-const Combo = ({ placeholder = 'Combo box', defaultValue = '', options = [] }: ComboProps) => {
+const Combo = ({ placeholder = 'Combo box', defaultValue = '', options = [], error }: ComboProps) => {
   /* ======   variables   ====== */
   const ref = useRef<HTMLInputElement>(null);
   const [search, setSearch] = useState('');
   const [value, setValue] = useState<string>(defaultValue);
-  const label = useMemo(() => options.find((x) => x.value === value)?.label ?? '', [value]);
+  const memoOptions = useMemo(() => options.filter((x) => !x.hidden), [options]);
+  const memoSearchOptions = useMemo(() => memoOptions.filter((x) => x.label.includes(search)), [options, search]);
+  const label = useMemo(() => options.find((x) => x.value === value)?.label ?? '', [value, options]);
   /* ======   function    ====== */
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => setSearch(e.currentTarget.value);
   const handleClick = (value: string) => setValue(value);
@@ -48,24 +50,25 @@ const Combo = ({ placeholder = 'Combo box', defaultValue = '', options = [] }: C
       <Input
         type="search"
         ref={ref}
+        error={error}
         placeholder="검색어를 입력하세요."
         onChange={handleChange}
         onClick={handleInputClick}
       />
       <div
-        className={`flex flex-col gap-1 after:block after:text-center [&:empty::after]:content-['검색결과가_없습니다.']`}
+        className={`flex flex-col gap-1 after:block after:text-center [&:empty::after]:content-[attr(data-text)]`}
+        data-text={memoOptions.length ? '검색결과가 없습니다.' : '비었습니다.'}
       >
-        {options
-          .filter((x) => x.label.includes(search))
-          .map((x) => (
-            <Button
-              themeSize={'xs'}
-              themeColor={x.value === value ? 'primary' : 'secondary'}
-              onClick={() => handleClick(x.value)}
-            >
-              {x.label}
-            </Button>
-          ))}
+        {memoSearchOptions.map((x) => (
+          <Button
+            themeSize={'xs'}
+            disabled={x.disabled}
+            themeColor={x.value === value ? 'primary' : 'secondary'}
+            onClick={() => handleClick(x.value)}
+          >
+            {x.label}
+          </Button>
+        ))}
       </div>
     </Menu>
   );
