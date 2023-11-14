cee34aa108a0e142fe603e61633cd362806fb95e
changwonyoon <cwyoon@semi-ts.com>
2023-11-14 10:31:53 +0900

    chore: 불필요 태그 제거

--- apps/conveyor/frontend/src/pages/Control/Page.tsx
+++ apps/conveyor/frontend/src/pages/Control/Page.tsx
@@ -20,30 +20,28 @@ const Control = () => {
   logger('render');
   return (
     <>
-      <div className="">
-        <h2 className="text-xl font-semibold text-gray-800 mb-4">Server Control</h2>
-        <div className="bg-white p-4 rounded-lg border border-gray-200 mb-6">
-          <Table
-            thead={['sid', 'name', 'status', 'version', 'StartedTime']}
-            data={serverData}
-            makePagination={false}
-            makeColumnSelect={false}
-            renderSelectComponent={<ServerSelect />}
-            renderSubComponent={<ServerSub />}
-          ></Table>
-        </div>
+      <h2 className="text-xl font-semibold text-gray-800 mb-4">Server Control</h2>
+      <div className="bg-white p-4 rounded-lg border border-gray-200 mb-6">
+        <Table
+          thead={['sid', 'name', 'status', 'version', 'StartedTime']}
+          data={serverData}
+          makePagination={false}
+          makeColumnSelect={false}
+          renderSelectComponent={<ServerSelect />}
+          renderSubComponent={<ServerSub />}
+        ></Table>
+      </div>
 
-        <h2 className="text-xl font-semibold text-gray-800 mb-4">TCM Control</h2>
-        <div className="bg-white p-4 rounded-lg border border-gray-200">
-          <Table
-            thead={['tid', 'status', 'version', 'StartedTime', 'AdjTCMConnection']}
-            data={tcmData}
-            makePagination={true}
-            makeColumnSelect={false}
-            renderSelectComponent={<TcmSelect />}
-            renderSubComponent={<TcmSub />}
-          ></Table>
-        </div>
+      <h2 className="text-xl font-semibold text-gray-800 mb-4">TCM Control</h2>
+      <div className="bg-white p-4 rounded-lg border border-gray-200">
+        <Table
+          thead={['tid', 'status', 'version', 'StartedTime', 'AdjTCMConnection']}
+          data={tcmData}
+          makePagination={true}
+          makeColumnSelect={false}
+          renderSelectComponent={<TcmSelect />}
+          renderSubComponent={<TcmSub />}
+        ></Table>
       </div>
     </>
   );
