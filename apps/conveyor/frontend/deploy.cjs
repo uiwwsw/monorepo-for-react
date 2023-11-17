5e73061f7e7f7f675b18bf831f5a4e3c4b878b7a
changwonyoon <cwyoon@semi-ts.com>
2023-11-17 15:09:46 +0900

    feat: 배포작업 준비

--- apps/conveyor/frontend/deploy.cjs
+++ apps/conveyor/frontend/deploy.cjs
@@ -14,19 +14,17 @@ async function copyDir(src, dest) {
     let srcPath = path.join(src, entry.name);
     let destPath = path.join(dest, entry.name);
 
-    entry.isDirectory() ?
-      await copyDir(srcPath, destPath) :
-      await copyFile(srcPath, destPath);
+    entry.isDirectory() ? await copyDir(srcPath, destPath) : await copyFile(srcPath, destPath);
   }
 }
 async function deleteDir(dir) {
-    try {
-      await fs.rm(dir, { recursive: true, force: true });
-    } catch (err) {
-      console.error(`Error deleting directory ${dir}:`, err);
-    }
+  try {
+    await fs.rm(dir, { recursive: true, force: true });
+  } catch (err) {
+    console.error(`Error deleting directory ${dir}:`, err);
   }
-  
+}
+
 async function main() {
   try {
     await deleteDir('./deploy');
@@ -37,9 +35,10 @@ async function main() {
     // dist 폴더와 index.js 파일 복사
     await copyDir('./apps/conveyor/frontend/dist', './deploy/dist');
     await copyFile('./apps/conveyor/frontend/index.js', './deploy/index.js');
+    await copyFile('./apps/conveyor/frontend/package-deploy.json', './deploy/package.json');
 
     // 종속성 설치
-    execSync('cd deploy && npm init -y && npm install express path', { stdio: 'inherit' });
+    execSync('cd ./deploy && npm i', { stdio: 'inherit' });
 
     console.log('Deployment preparation complete.');
   } catch (error) {
