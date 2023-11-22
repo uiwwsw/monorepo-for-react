098811aed7dc32112ef92e17a325dc4154181706
Jimmy Kim <jimmeykim@gmail.com>
2023-11-22 11:18:15 +0900

    feature/be: ZoneStats Api 추가

--- packages/backend/types/src/stats/domain.ts
+++ packages/backend/types/src/stats/domain.ts
@@ -1,3 +1,4 @@
+import { IAlarminfoRow } from "../data";
 
 // 반송 지표 요청
 export interface CarrierStatsInRequest {
@@ -24,4 +25,36 @@ export interface CarrierStatsRow {
 export interface CarrierStatsResponse {
     rows : CarrierStatsRow[];
     total_count : number;
+}
+
+// 알람 지표 요청
+export interface AlarmStatsInRequest {
+    start_time : Date;
+    end_time : Date;
+    page : number;
+    page_size? : number; // default 30
+    find_key? : string;
+}
+// 알람 지표 응답
+export interface AlarmStatsResponse {
+    rows : IAlarminfoRow[];
+    total_count : number;
+}
+
+
+// Zone 지표 요청
+export interface ZoneStatsInRequest {
+    start_time : Date;
+    end_time : Date;
+}
+export interface ZoneStatsItem {
+    date? : string;
+    zoneId? : number;
+    carrierNum? : number;
+    alarmNum? : number;
+    warningNum? : number;
+}
+// Zone 지표 응답
+export interface ZoneStatsResponse {
+    rows : ZoneStatsItem[];
 }
