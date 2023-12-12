f9e6b64bfebb2fa232f261252fecb60a9681ec04
changwonyoon <cwyoon@semi-ts.com>
2023-12-12 12:51:07 +0900

    feat: domain 개선

--- apps/conveyor/frontend/src/libs/auth/domain.ts
+++ apps/conveyor/frontend/src/libs/auth/domain.ts
@@ -1,4 +1,5 @@
 import { IUserRow, SignInResponse, UserGrade } from '@package-backend/types';
+import { FORMAT, newDate } from '@package-frontend/utils';
 
 export class Auth {
   uid: number;
@@ -11,7 +12,7 @@ export class Auth {
     this.userName = username;
     this.grade = grade;
     this.token = token;
-    this.lastAccess = `${last_access}`;
+    this.lastAccess = last_access ? newDate(last_access).format(FORMAT) : '';
   }
 }
 export class User {
@@ -20,15 +21,15 @@ export class User {
   userName?: string;
   grade?: UserGrade;
   gradeName?: string;
-  createdDate?: string;
-  lastAccess?: string;
+  createdDate: string;
+  lastAccess: string;
   constructor({ uid, user_id, user_name, grade, created_date, last_access }: IUserRow) {
     this.uid = uid;
     this.userId = user_id;
     this.userName = user_name;
     this.grade = grade;
     this.gradeName = grade ? UserGrade[grade] : 'unknown';
-    this.createdDate = `${created_date}`;
-    this.lastAccess = `${last_access}`;
+    this.createdDate = created_date ? newDate(created_date).format(FORMAT) : '';
+    this.lastAccess = last_access ? newDate(last_access).format(FORMAT) : '';
   }
 }
