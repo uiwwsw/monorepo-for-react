399f9d45b93949d1e8e0d711d9572a33103d51fc
changwonyoon <cwyoon@semi-ts.com>
2023-12-18 17:14:03 +0900

    chore: 세팅 옵션 확장

--- apps/conveyor/frontend/src/utils/constants.ts
+++ apps/conveyor/frontend/src/utils/constants.ts
@@ -6,4 +6,9 @@ export const pageSizeOptions = [
   { value: '30', label: t('30개씩 보기') },
   { value: '40', label: t('40개씩 보기') },
   { value: '50', label: t('50개씩 보기') },
+  { value: '60', label: t('60개씩 보기') },
+  { value: '70', label: t('70개씩 보기') },
+  { value: '80', label: t('80개씩 보기') },
+  { value: '90', label: t('90개씩 보기') },
+  { value: '100', label: t('100개씩 보기') },
 ];
